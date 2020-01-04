42331ef1cf84eb49c28a43d20d52b649c9b455bd
Clay Harris <rcharris42@gmail.com>
2020-01-04 14:31:28 -0800

    Challenge 8 complete!

--- src/main/java/com/claydevelops/projecteuler/App.java
+++ src/main/java/com/claydevelops/projecteuler/App.java
@@ -1,10 +1,10 @@
 package com.claydevelops.projecteuler;
 
-import com.claydevelops.projecteuler.challenges.Challenge007;
+import com.claydevelops.projecteuler.challenges.Challenge008;
 
 public class App {
     public static void main(String[] args) {
-        Runnable challenge = new Challenge007();
+        Runnable challenge = new Challenge008();
 
         challenge.run();
 
